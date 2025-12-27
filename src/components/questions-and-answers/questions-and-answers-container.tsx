88829dbe328769ced8009b9f87598cf846c23913
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2025-12-27 13:00:37 +0200

    feat: introduce new radial shadow and decor image to questions section and remove unused style from services slider.

--- src/components/questions-and-answers/questions-and-answers-container.tsx
+++ src/components/questions-and-answers/questions-and-answers-container.tsx
@@ -1,12 +1,23 @@
 import QuestionsAccordion from './questions-accordion';
+import QuestionsRadialShadow from './qustions-radial-shadow';
+import Image from 'next/image';
 
 const QuestionsAndAnswersContainer = () => {
   return (
     <section className="relative z-10 px-[20px] pt-[148px]">
+      <QuestionsRadialShadow className="absolute top-[-120px] left-[-440px] max-w-none -z-20 pointer-events-none" />
       <h2 className="max-w-[240px] mb-[48px] font-manrope font-light text-[40px] leading-[120%] text-white uppercase [text-shadow:4px_3px_9px_rgba(255,0,166,0.2)]">
         Spørgsmål og svar
       </h2>
       <QuestionsAccordion />
+      <Image
+        src="/questions-right-decor.png"
+        alt="questions-right-decor"
+        quality={80}
+        width={1873}
+        height={1873}
+        className="absolute top-[-240px] right-[-980px] max-w-none -z-30 pointer-events-none"
+      />
     </section>
   );
 };
