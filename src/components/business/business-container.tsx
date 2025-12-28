6d58eefb2e2766f3ccfaaec8bb8f9cd9984aeb6a
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2025-12-28 22:50:12 +0200

    fix: Remove `mix-blend-screen` class from image component.

--- src/components/business/business-container.tsx
+++ src/components/business/business-container.tsx
@@ -39,7 +39,7 @@ const BusinessContainer = () => {
         height={1257}
         sizes="(max-width: 768px) 100vw, 1257px"
         quality={100}
-        className="absolute top-[-30px] right-[-750px] max-w-none pointer-events-none -z-20 mix-blend-screen contrast-125 saturate-150"
+        className="absolute top-[-30px] right-[-750px] max-w-none pointer-events-none -z-20 contrast-125 saturate-150"
       />
 
       <Image
